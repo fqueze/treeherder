3cdc39ade0ce1398f4ad8decfbfce7f683b12db6
Sarah Clements <sclements313@gmail.com>
2019-05-14 18:04:20 -0700

    add reset button condition

--- ui/perfherder/alerts/AlertTableControls.jsx
+++ ui/perfherder/alerts/AlertTableControls.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Button } from 'reactstrap';
 
 import SimpleTooltip from '../../shared/SimpleTooltip';
-import alertStatus from '../constants';
+import { alertStatus } from '../constants';
 
 export default class AlertTableControls extends React.Component {
   constructor(props) {
@@ -11,28 +11,28 @@ export default class AlertTableControls extends React.Component {
     this.state = {};
   }
 
-  // anySelectedAndTriaged = alerts =>
-  // alerts
-  //   .map(
-  //     alert => alert.status === alertStatus.untriaged)
-  //   .some(x => x);
+  anyUntriaged = () =>
+    this.props.selectedAlerts.some(
+      alert => alert.status === alertStatus.untriaged,
+    );
 
+  // TODO add reset onclick functionality
   render() {
     const { selectedAlerts } = this.props;
-    console.log(selectedAlerts);
+
     return (
       <React.Fragment>
-        {/* {selectedAlerts.some(alert => alert.status === alertStatus.untriaged) && ( */}
-        <SimpleTooltip
-          text={
-            <Button color="warning" onClick={() => {}}>
-              {' '}
-              Reset
-            </Button>
-          }
-          tooltipText="Reset selected alerts to untriaged"
-        />
-        {/* )} */}
+        {this.anyUntriaged() && (
+          <SimpleTooltip
+            text={
+              <Button color="warning" onClick={() => {}}>
+                {' '}
+                Reset
+              </Button>
+            }
+            tooltipText="Reset selected alerts to untriaged"
+          />
+        )}
       </React.Fragment>
     );
   }
